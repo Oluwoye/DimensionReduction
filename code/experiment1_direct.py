d76c851a3a99ee92f3f7c9edba9fd7165ea2d85b
Oluwoye <Tim.Cech@student.hpi.uni-potsdam.de>
2022-02-24 16:50:44 +0100

    Initial experiment with additional outlier functionality

--- code/experiment1_direct.py
+++ code/experiment1_direct.py
@@ -3,6 +3,10 @@
 
 import os
 import warnings
+
+from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
+
 warnings.filterwarnings('ignore')
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -26,6 +30,7 @@ import metrics
 import ssnp
 import nnproj
 
+
 def compute_all_metrics(X, X_2d, D_high, D_low, y, X_inv=None):
     T = metrics.metric_trustworthiness(X, X_2d, D_high, D_low)
     C = metrics.metric_continuity(X, X_2d, D_high, D_low)
@@ -37,8 +42,9 @@ def compute_all_metrics(X, X_2d, D_high, D_low, y, X_inv=None):
         MSE = metrics.metric_mse(X, X_inv)
     else:
         MSE = -99.0
-    
+
     return T, C, R, S, N, MSE
+
 
 def plot(X, y, figname=None):
     if len(np.unique(y)) <= 10:
@@ -47,9 +53,9 @@ def plot(X, y, figname=None):
         cmap = plt.get_cmap('tab20')
 
     fig, ax = plt.subplots(figsize=(20, 20))
-    
+
     for cl in np.unique(y):
-        ax.scatter(X[y==cl,0], X[y==cl,1], c=[cmap(cl)], label=cl, s=20)
+        ax.scatter(X[y == cl, 0], X[y == cl, 1], c=[cmap(cl)], label=cl, s=20)
         ax.axis('off')
 
     if figname is not None:
@@ -60,10 +66,16 @@ def plot(X, y, figname=None):
     del ax
 
 
+def cantor_pairing(y1, y2):
+    y_res = np.array(
+        [int(0.5 * (y1[i] + y2[i]) * (y1[i] + y2[i] + 1) + y2[i]) for i in range(0, len(y2))])
+    return y_res
+
+
 if __name__ == '__main__':
     patience = 5
     epochs = 200
-    
+
     min_delta = 0.05
 
     verbose = False
@@ -74,7 +86,7 @@ if __name__ == '__main__':
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    data_dir ='../data'
+    data_dir = '../data'
     data_dirs = ['mnist', 'fashionmnist', 'har', 'reuters']
 
     epochs_dataset = {}
@@ -104,7 +116,7 @@ if __name__ == '__main__':
         n_classes = len(np.unique(y)) * classes_mult[dataset_name]
         n_samples = X.shape[0]
 
-        train_size = min(int(n_samples*0.9), 5000)
+        train_size = min(int(n_samples * 0.9), 5000)
 
         X_train, _, y_train, _ = train_test_split(X, y, train_size=train_size, random_state=420, stratify=y)
         D_high = metrics.compute_distance_list(X_train)
@@ -121,11 +133,55 @@ if __name__ == '__main__':
         ssnpkm.fit(X_train, y_km)
         X_ssnpkm = ssnpkm.transform(X_train)
 
+        ssnpif = ssnp.SSNP(epochs=epochs, verbose=verbose, patience=0, opt='adam', bottleneck_activation='linear')
+        outlier_model = IsolationForest()
+        y_if = outlier_model.fit_predict(X_train)
+        y_if = np.array([0 if el == -1 else el for el in y_if])
+        ssnpif.fit(X_train, y_if)
+        X_ssnpif = ssnpif.transform(X_train)
+
+        ssnpkmif = ssnp.SSNP(epochs=epochs, verbose=verbose, patience=0, opt='adam', bottleneck_activation='linear')
+        y_res_kmif = cantor_pairing(y_km, y_if)
+        ssnpkmif.fit(X_train, y_res_kmif)
+        X_ssnpkmif = ssnpkmif.transform(X_train)
+
         ssnpag = ssnp.SSNP(epochs=epochs, verbose=verbose, patience=0, opt='adam', bottleneck_activation='linear')
         C = AgglomerativeClustering(n_clusters=n_classes)
         y_ag = C.fit_predict(X_train)
         ssnpag.fit(X_train, y_ag)
         X_ssnpag = ssnpag.transform(X_train)
+
+        ssnpagif = ssnp.SSNP(epochs=epochs, verbose=verbose, patience=0, opt='adam', bottleneck_activation='linear')
+        y_res_agif = cantor_pairing(y_ag, y_if)
+        ssnpagif.fit(X_train, y_res_agif)
+        X_ssnpagif = ssnpagif.transform(X_train)
+
+        ssnpkmagif = ssnp.SSNP(epochs=epochs, verbose=verbose, patience=0, opt='adam', bottleneck_activation='linear')
+        y_res_kmagif = cantor_pairing(y_res_kmif, y_ag)
+        ssnpkmagif.fit(X_train, y_res_kmagif)
+        X_ssnpkmagif = ssnpkmagif.transform(X_train)
+
+        ssnplof = ssnp.SSNP(epochs=epochs, verbose=verbose, patience=0, opt='adam', bottleneck_activation='linear')
+        outlier_model = LocalOutlierFactor()
+        y_lof = outlier_model.fit_predict(X_train)
+        y_lof = np.array([0 if el == -1 else el for el in y_lof])
+        ssnplof.fit(X_train, y_lof)
+        X_ssnplof = ssnplof.transform(X_train)
+
+        ssnpkmlof = ssnp.SSNP(epochs=epochs, verbose=verbose, patience=0, opt='adam', bottleneck_activation='linear')
+        y_res_kmlof = cantor_pairing(y_km, y_lof)
+        ssnpkmlof.fit(X_train, y_res_kmlof)
+        X_ssnpkmlof = ssnpkmlof.transform(X_train)
+
+        ssnpaglof = ssnp.SSNP(epochs=epochs, verbose=verbose, patience=0, opt='adam', bottleneck_activation='linear')
+        y_res_aglof = cantor_pairing(y_ag, y_lof)
+        ssnpaglof.fit(X_train, y_res_aglof)
+        X_ssnpaglof = ssnpaglof.transform(X_train)
+
+        ssnpkmaglof = ssnp.SSNP(epochs=epochs, verbose=verbose, patience=0, opt='adam', bottleneck_activation='linear')
+        y_res_kmaglof = cantor_pairing(y_res_kmlof, y_ag)
+        ssnpkmaglof.fit(X_train, y_res_kmaglof)
+        X_ssnpkmaglof = ssnpkmaglof.transform(X_train)
 
         tsne = TSNE(n_jobs=4, random_state=420)
         X_tsne = tsne.fit_transform(X_train)
@@ -143,55 +199,88 @@ if __name__ == '__main__':
 
         D_ssnpgt = metrics.compute_distance_list(X_ssnpgt)
         D_ssnpkm = metrics.compute_distance_list(X_ssnpkm)
+        D_ssnpif = metrics.compute_distance_list(X_ssnpif)
+        D_ssnpkmif = metrics.compute_distance_list(X_ssnpkmif)
         D_ssnpag = metrics.compute_distance_list(X_ssnpag)
+        D_ssnpagif = metrics.compute_distance_list(X_ssnpagif)
+        D_ssnpkmagif = metrics.compute_distance_list(X_ssnpkmagif)
+        D_ssnplof = metrics.compute_distance_list(X_ssnplof)
+        D_ssnpkmlof = metrics.compute_distance_list(X_ssnpkmlof)
+        D_ssnpaglof = metrics.compute_distance_list(X_ssnpaglof)
+        D_ssnpkmaglof = metrics.compute_distance_list(X_ssnpkmaglof)
         D_tsne = metrics.compute_distance_list(X_tsne)
         D_umap = metrics.compute_distance_list(X_umap)
         D_aep = metrics.compute_distance_list(X_aep)
         D_nnp = metrics.compute_distance_list(X_nnp)
 
         results.append((dataset_name, 'SSNP-GT',) + compute_all_metrics(X_train, X_ssnpgt, D_high, D_ssnpgt, y_train))
-        results.append((dataset_name, 'SSNP-KMeans',) + compute_all_metrics(X_train, X_ssnpkm, D_high, D_ssnpkm, y_train))
+        results.append(
+            (dataset_name, 'SSNP-KMeans',) + compute_all_metrics(X_train, X_ssnpkm, D_high, D_ssnpkm, y_train))
+        results.append(
+            (dataset_name, 'SSNP-IF',) + compute_all_metrics(X_train, X_ssnpif, D_high, D_ssnpif, y_train))
+        results.append(
+            (dataset_name, 'SSNP-KMeans-IF',) + compute_all_metrics(X_train, X_ssnpkmif, D_high, D_ssnpkmif, y_train))
         results.append((dataset_name, 'SSNP-AG',) + compute_all_metrics(X_train, X_ssnpag, D_high, D_ssnpag, y_train))
+        results.append(
+            (dataset_name, 'SSNP-AG-IF',) + compute_all_metrics(X_train, X_ssnpagif, D_high, D_ssnpagif, y_train))
+        results.append(
+            (dataset_name, 'SSNP-KM-AG-IF',) + compute_all_metrics(X_train, X_ssnpkmagif, D_high, D_ssnpkmagif,
+                                                                   y_train))
+        results.append(
+            (dataset_name, 'SSNP-LOF',) + compute_all_metrics(X_train, X_ssnplof, D_high, D_ssnplof, y_train))
+        results.append(
+            (dataset_name, 'SSNP-KMeans-LOF',) + compute_all_metrics(X_train, X_ssnpkmlof, D_high, D_ssnpkmlof,
+                                                                     y_train))
+        results.append(
+            (dataset_name, 'SSNP-AG-LOF',) + compute_all_metrics(X_train, X_ssnpaglof, D_high, D_ssnpaglof, y_train))
+        results.append(
+            (dataset_name, 'SSNP-KM-AG-LOF',) + compute_all_metrics(X_train, X_ssnpkmaglof, D_high, D_ssnpkmaglof,
+                                                                    y_train))
         results.append((dataset_name, 'AE',) + compute_all_metrics(X_train, X_aep, D_high, D_aep, y_train))
         results.append((dataset_name, 'TSNE',) + compute_all_metrics(X_train, X_tsne, D_high, D_tsne, y_train))
         results.append((dataset_name, 'UMAP',) + compute_all_metrics(X_train, X_umap, D_high, D_umap, y_train))
         results.append((dataset_name, 'NNP',) + compute_all_metrics(X_train, X_nnp, D_high, D_nnp, y_train))
 
-        for X_, label in zip([X_ssnpgt, X_ssnpkm, X_ssnpag, X_umap, X_tsne, X_aep, X_nnp], ['SSNP-GT', 'SSNP-KMeans', 'SSNP-AG', 'UMAP', 'TSNE', 'AE', 'NNP']):
+        for X_, label in zip([X_ssnpgt, X_ssnpkm, X_ssnpif, X_ssnpkmif, X_ssnpag, X_ssnpagif, X_ssnpkmagif,
+                              X_ssnplof, X_ssnpkmlof, X_ssnpaglof, X_ssnpkmaglof, X_umap, X_tsne, X_aep, X_nnp],
+                             ['SSNP-GT', 'SSNP-KMeans', 'SSNP-IF', 'SSNP-KMeans-IF', 'SSNP-AG', 'SSNP-AG-IF',
+                              'SSNP-KM-AG-IF',
+                              'SSNP-LOF', 'SSNP-KMeans-LOF', 'SSNP-AG-LOF', 'SSNP-KM-AG-LOF', 'UMAP', 'TSNE', 'AE',
+                              'NNP']):
             fname = os.path.join(output_dir, '{0}_{1}.png'.format(dataset_name, label))
             print(fname)
             plot(X_, y_train, fname)
 
-    df = pd.DataFrame(results, columns=[    'dataset_name',
-                                            'test_name',
-                                            'T_train',
-                                            'C_train',
-                                            'R_train',
-                                            'S_train',
-                                            'N_train',
-                                            'MSE_train'])
+    df = pd.DataFrame(results, columns=['dataset_name',
+                                        'test_name',
+                                        'T_train',
+                                        'C_train',
+                                        'R_train',
+                                        'S_train',
+                                        'N_train',
+                                        'MSE_train'])
 
-    df.to_csv(os.path.join(output_dir, 'metrics.csv'), header=True, index=None)
+    df.to_csv(os.path.join(output_dir, 'metrics.csv'), header=True, index=False)
 
-    #don't plot NNP
+    # don't plot NNP
     font = ImageFont.truetype("/usr/share/fonts/dejavu/DejaVuSans.ttf", 50)
     pri_images = ['SSNP-KMeans', 'SSNP-AG', 'AE', 'TSNE', 'UMAP', 'SSNP-GT']
 
-    images = glob(output_dir + '/*.png')    
+    images = glob(output_dir + '/*.png')
     base = 2000
 
     for d in data_dirs:
         dataset_name = d
-        to_paste = []    
+        to_paste = []
 
         for i, label in enumerate(pri_images):
             to_paste += [f for f in images if os.path.basename(f) == '{0}_{1}.png'.format(dataset_name, label)]
 
-        img = np.zeros((base, base*6, 3)).astype('uint8')
-        
+        img = np.zeros((base, base * 6, 3)).astype('uint8')
+
         for i, im in enumerate(to_paste):
             tmp = io.imread(im)
-            img[:,i*base:(i+1)*base,:] = tmp[:,:,:3]
+            img[:, i * base:(i + 1) * base, :] = tmp[:, :, :3]
 
         pimg = Image.fromarray(img)
         pimg.save(output_dir + '/composite_full_{0}.png'.format(dataset_name))
@@ -199,25 +288,24 @@ if __name__ == '__main__':
         for i, label in enumerate(pri_images):
             print('/composite_full_{0}.png'.format(dataset_name), "{0} {1}".format(dataset_name, label))
 
-
     font = ImageFont.truetype("/usr/share/fonts/dejavu/DejaVuSans.ttf", 50)
     pri_images = ['SSNP-KMeans', 'SSNP-AG', 'AE']
 
-    images = glob(output_dir + '/*.png')    
+    images = glob(output_dir + '/*.png')
     base = 2000
 
     for d in data_dirs:
         dataset_name = d
-        to_paste = []    
+        to_paste = []
 
         for i, label in enumerate(pri_images):
             to_paste += [f for f in images if os.path.basename(f) == '{0}_{1}.png'.format(dataset_name, label)]
 
-        img = np.zeros((base, base*3, 3)).astype('uint8')
-        
+        img = np.zeros((base, base * 3, 3)).astype('uint8')
+
         for i, im in enumerate(to_paste):
             tmp = io.imread(im)
-            img[:,i*base:(i+1)*base,:] = tmp[:,:,:3]
+            img[:, i * base:(i + 1) * base, :] = tmp[:, :, :3]
 
         pimg = Image.fromarray(img)
         pimg.save(output_dir + '/composite_{0}.png'.format(dataset_name))
--- code/metrics.py
+++ code/metrics.py
@@ -10,8 +10,10 @@ from scipy import optimize
 from scipy.sparse import csr_matrix
 from sklearn.neighbors import NearestNeighbors
 
+
 def compute_distance_list(X):
     return spatial.distance.pdist(X, 'euclidean')
+
 
 def metric_neighborhood_hit(X, y, k=7):
     knn = KNeighborsClassifier(n_neighbors=k)
@@ -20,9 +22,10 @@ def metric_neighborhood_hit(X, y, k=7):
     neighbors = knn.kneighbors(X, return_distance=False)
     return np.mean(np.mean((y[neighbors] == np.tile(y.reshape((-1, 1)), k)).astype('uint8'), axis=1))
 
+
 def metric_trustworthiness(X_high, X_low, D_high_m, D_low_m, k=7):
     D_high = spatial.distance.squareform(D_high_m)
-    D_low  = spatial.distance.squareform(D_low_m)
+    D_low = spatial.distance.squareform(D_low_m)
 
     n = X_high.shape[0]
 
@@ -48,7 +51,7 @@ def metric_trustworthiness(X_high, X_low, D_high_m, D_low_m, k=7):
 
 def metric_continuity(X_high, X_low, D_high_l, D_low_l, k=7):
     D_high = spatial.distance.squareform(D_high_l)
-    D_low  = spatial.distance.squareform(D_low_l)
+    D_low = spatial.distance.squareform(D_low_l)
 
     n = X_high.shape[0]
 
@@ -71,12 +74,14 @@ def metric_continuity(X_high, X_low, D_high_l, D_low_l, k=7):
 
     return float((1 - (2 / (n * k * (2 * n - 3 * k - 1)) * sum_i)).squeeze())
 
+
 def metric_shepard_diagram_correlation(D_high, D_low):
     return stats.spearmanr(D_high, D_low)[0]
 
 
 def metric_normalized_stress(D_high, D_low):
-    return np.sum((D_high - D_low)**2) / np.sum(D_high**2)
+    return np.sum((D_high - D_low) ** 2) / np.sum(D_high ** 2)
+
 
 def metric_mse(X, X_hat):
     return np.mean(np.square(X - X_hat))
